0c7909cefae9bfa488b64479099457a95d1d9925
AzureAD\DattuPatel <dattupat@kevgroup.com>
2026-02-15 19:05:12 -0500

    Update sandbox for KEV-UI 0.1.0 consumption
    - Register all available package locale translations in i18n.ts
    - Update index.css to use self-sufficient design-system plugin
    - Update lockfile for 0.1.0 package versions

--- react/src/i18n.ts
+++ react/src/i18n.ts
@@ -1,11 +1,20 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { registerTypographyTranslations } from '@kev-ui/typography/locales';
+import { registerAvatarTranslations } from '@kev-ui/avatar/locales';
+import { registerBackdropTranslations } from '@kev-ui/backdrop/locales';
+import { registerBadgeTranslations } from '@kev-ui/badge/locales';
+import { registerButtonTranslations } from '@kev-ui/button/locales';
 import { registerChipTranslations } from '@kev-ui/chip/locales';
-import { registerFormFieldTranslations } from '@kev-ui/form-field/locales';
-import { registerDropdownMenuTranslations } from '@kev-ui/dropdown-menu/locales';
-import { registerListTranslations } from '@kev-ui/list/locales';
 import { registerDrawerTranslations } from '@kev-ui/drawer/locales';
+import { registerDropdownMenuTranslations } from '@kev-ui/dropdown-menu/locales';
+import { registerFormTranslations } from '@kev-ui/form/locales';
+import { registerFormFieldTranslations } from '@kev-ui/form-field/locales';
+import { registerFormUtilitiesTranslations } from '@kev-ui/form-utilities/locales';
+import { registerFormValidationTranslations } from '@kev-ui/form-validation/locales';
+import { registerListTranslations } from '@kev-ui/list/locales';
+import { registerModalTranslations } from '@kev-ui/modal/locales';
+import { registerScaffoldingTranslations } from '@kev-ui/scaffolding/locales';
+import { registerTypographyTranslations } from '@kev-ui/typography/locales';
 
 i18next.use(initReactI18next).init({
   lng: 'en-CA',
@@ -17,11 +26,20 @@ i18next.use(initReactI18next).init({
   resources: {},
 });
 
-registerTypographyTranslations(i18next);
+registerAvatarTranslations(i18next);
+registerBackdropTranslations(i18next);
+registerBadgeTranslations(i18next);
+registerButtonTranslations(i18next);
 registerChipTranslations(i18next);
-registerFormFieldTranslations(i18next);
-registerDropdownMenuTranslations(i18next);
-registerListTranslations(i18next);
 registerDrawerTranslations(i18next);
+registerDropdownMenuTranslations(i18next);
+registerFormTranslations(i18next);
+registerFormFieldTranslations(i18next);
+registerFormUtilitiesTranslations(i18next);
+registerFormValidationTranslations(i18next);
+registerListTranslations(i18next);
+registerModalTranslations(i18next);
+registerScaffoldingTranslations(i18next);
+registerTypographyTranslations(i18next);
 
 export default i18next;
